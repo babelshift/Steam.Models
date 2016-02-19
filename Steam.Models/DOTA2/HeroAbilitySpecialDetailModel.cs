774e5864193c92e7ab136713d4b0c72559eee142
babelshift <justin.d.skiles@gmail.com>
2016-02-19 10:26:07 -0500

    Added hero ability detail model. Removed memory cache key. (moved it to DotaDb.Data lib since that's where caching occurs)

---
+++ Steam.Models/DOTA2/HeroAbilitySpecialDetailModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Steam.Models.DOTA2
+{
+    public class HeroAbilitySpecialDetailModel
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public string RawName { get; set; }
+    }
+}
