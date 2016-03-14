1596a20b6073dd946e45581d18331b5d123c5000
babelshift <justin.d.skiles@gmail.com>
2016-03-14 10:14:45 -0400

    Updated libs. Adding models for steam store endpoints.

---
+++ Steam.Models/SteamStore/StoreMacRequirementsModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Steam.Models.SteamStore
+{
+    public class StoreMacRequirementsModel
+    {
+        public string Minimum { get; set; }
+    }
+}
