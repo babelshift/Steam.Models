4a28909d3b2fe4d028a2047d660845998f777851
babelshift <justin.d.skiles@gmail.com>
2016-03-15 11:48:45 -0400

    Starting to add some missing localized name properties.

--- Steam.Models/DOTA2/GameItemModel.cs
+++ Steam.Models/DOTA2/GameItemModel.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public int Cost { get; set; }
         public string Name { get; set; }
+        public string LocalizedName { get; set; }
         public bool IsRecipe { get; set; }
         public bool IsAvailableAtSecretShop { get; set; }
         public bool IsAvailableAtSideShop { get; set; }
--- Steam.Models/DOTA2/HeroModel.cs
+++ Steam.Models/DOTA2/HeroModel.cs
@@ -2,7 +2,8 @@
 {
     public class HeroModel
     {
-        public string Name { get; set; }
         public int Id { get; set; }
+        public string Name { get; set; }
+        public string LocalizedName { get; set; }
     }
 }
