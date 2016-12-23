83e1fd1e339672833b943ca8a148f5015eb9988e
Justin <justin.d.skiles@gmail.com>
2016-12-23 15:11:59 -0500

    Added TradeOffersModel for GetTradeOffers endpoint.

---
+++ Steam.Models/SteamEconomy/TradeOffersModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Steam.Models.SteamEconomy
+{
+    public class TradeOffersModel
+    {
+        public IList<TradeOfferModel> TradeOffersSent { get; set; }
+        public IList<TradeOfferModel> TradeOffersReceived { get; set; }
+        public IList<string> Descriptions { get; set; }
+    }
+}
