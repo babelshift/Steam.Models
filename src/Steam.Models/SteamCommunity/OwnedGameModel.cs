b80f67ccecc470d7f32ada13101a7ebe9f23b3b8
Justin Skiles <justin.d.skiles@gmail.com>
2017-02-20 10:41:33 -0500

    Playtime for owned games in TimeSpan.

--- src/Steam.Models/SteamCommunity/OwnedGameModel.cs
+++ src/Steam.Models/SteamCommunity/OwnedGameModel.cs
@@ -1,4 +1,6 @@
-﻿namespace Steam.Models.SteamCommunity
+﻿using System;
+
+namespace Steam.Models.SteamCommunity
 {
     public class OwnedGameModel
     {
@@ -6,7 +8,7 @@
 
         public string Name { get; set; }
 
-        public uint PlaytimeForever { get; set; }
+        public TimeSpan PlaytimeForever { get; set; }
 
         public string ImgIconUrl { get; set; }
 
@@ -14,6 +16,6 @@
 
         public bool HasCommunityVisibleStats { get; set; }
 
-        public uint? Playtime2weeks { get; set; }
+        public TimeSpan? PlaytimeLastTwoWeeks { get; set; }
     }
 }
